7826c004061966b4989d1917af7b82413770ba85
vinayakpotdar79 <vinayakpotdar7977@gmail.com>
2025-08-16 20:41:48 +0530

    added edit profile

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { registerUser, loginUser, getProfile } from '../controllers/authController.js';
 import { protect } from '../middleware/authMiddleware.js';
+import User from '../models/User.js';
 const router = express.Router();
 
 router.post('/register', registerUser);
